115d70762fdfdfe1b29d19515d97ae5b37cbdae0
Mehran Shahidi <m.mehrashahidi@gmail.com>
2021-08-22 16:45:37 +0430

    added projects

--- config/server.js
+++ config/server.js
@@ -1,9 +1,37 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  // socket: '/tmp/nginx.socket', // only use if absolutely required
+  // emitErrors: false,
+  // url: env('PUBLIC_URL', 'https://api.example.com'),
+  // proxy: env.bool('IS_PROXIED', true),
+  // cron: {
+  //   enabled: env.bool('CRON_ENABLED', false),
+  // },
   admin: {
     auth: {
-      secret: env("ADMIN_JWT_SECRET"),
+      events: {
+        onConnectionSuccess(e) {
+          console.log(e.user, e.provider);
+        },
+        onConnectionError(e) {
+          console.error(e.error, e.provider);
+        },
+      },
+      secret: env('ADMIN_JWT_SECRET', 'someSecretKey'),
     },
+    // url: env('PUBLIC_ADMIN_URL', '/dashboard'),
+    // autoOpen: false,
+    // watchIgnoreFiles: [
+    //   './my-custom-folder', // Folder
+    //   './scripts/someScript.sh', // File
+    // ],
+    // host: 'localhost', // Only used for --watch-admin
+    // port: 8003, // Only used for --watch-admin
+    // serveAdminPanel: env.bool('SERVE_ADMIN', true),
+    // forgotPassword: {
+    //   from: 'no-reply@example.com',
+    //   replyTo: 'no-reply@example.com',
+    // },
   },
 });
